eda4fcdfd59dbc54f6052716b967114fee6857d3
Rob Horton <robhorton@outlook.com>
2015-12-18 12:32:06 -0600

    Adding basic site with opening default url

--- gulp-basicsite/gulpfile.js
+++ gulp-basicsite/gulpfile.js
@@ -1,16 +1,42 @@
 
 // gulpfile for gulp-basicsite
 
-var gulp = require('gulp');						// Our task runner
-var gulp = require('gulp-connect')				// Opens a website in the browser
+var gulp = require('gulp');							// Our task runner
+var connect = require('gulp-connect');				// Runs a webserver
+var open = require('gulp-open');					// Opens a url in the browser
 
 var config = {
 	port: 9005,
-	devBaseUrl: 'http://localhost'
+	devBaseUrl: 'http://localhost',
+	paths: {
+		html: './src/*.html',
+		dist: './dist'
+	},
+	homepage: 'index.html'
 }
 
-gulp.task('basic', function() {
-	return console.log('basic task activated');
+gulp.task('html', function() {
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
 });
 
-gulp.task('default', ['basic']);
+gulp.task('startServer', function () {
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
+});
+
+gulp.task('openHomePage', ['startServer'], function () {
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+});
+
+gulp.task('default', ['html', 'openHomePage', 'watch']);
