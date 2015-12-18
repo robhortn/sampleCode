001adee5e51660f75ded6a81195569ff5b1d0f32
Rob Horton <rhorton@mtm-inc.net>
2015-12-18 10:43:45 -0600

    Refactored dir structure and added config section to gulpfile.js

--- gulp-bundle/dest/appScripts.js
+++
@@ -1,1 +0,0 @@
-!function(){console.log("doThat activated")}(),function(){console.log("doThis activated")}();
--- gulp-bundle/gulpfile.js
+++ gulp-bundle/gulpfile.js
@@ -5,11 +5,18 @@ var gulp = require('gulp');						// Our task runner
 var concat = require('gulp-concat');			// Concats files 
 var uglify = require('gulp-uglify');			// Removes unnecessary spaces from file(s)
 
-gulp.task('scripts', function(){
-	gulp.src('./js/*.js')						// Grab all js files using globbing pattern
-		.pipe(concat('appScripts.js'))			// Concat all files into one
-		.pipe(uglify())							// Uglify the file, removing all unnecessary space
-		.pipe(gulp.dest('./dest/'));			// Copy formatted file to the destination folder
+var config = {
+	paths: {
+		js: './src/**/*.js', 
+		dist: './dist'
+	}
+}
+
+gulp.task('js', function() {
+	gulp.src(config.paths.js)									// Grab all js files using globbing pattern
+		.pipe(concat('bundle.js'))								// Concat all files into one
+		.pipe(uglify())											// Uglify the file, removing all unnecessary space
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))		// Copy formatted file to the destination folder
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['js']);
